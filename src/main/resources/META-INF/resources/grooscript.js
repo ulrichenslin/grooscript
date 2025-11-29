a5f7865d8574aa5107fbc735800170f054336abe
ulrich <ulrich@i3.vc>
2025-11-29 12:50:48 +0200

    fix endsWith that fails if a string ends with '/'

--- src/main/resources/META-INF/resources/grooscript.js
+++ src/main/resources/META-INF/resources/grooscript.js
@@ -1779,7 +1779,6 @@
 
     String.prototype.endsWith = function(value) {
         return this.substring(this.length - value.length) === value;
-        // return this.indexOf(value) > -1 && this.indexOf(value) == (this.length - value.length);
     };
 
     String.prototype.count = function(value) {
